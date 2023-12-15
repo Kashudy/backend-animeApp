a27845c0e5b63b35b1093e3e620bd5cc345a5643
Kashudy <kashudyord@gmail.com>
2023-12-14 23:10:03 -0300

    adjusted database interface for testing

--- tests/UnitTest_1.py
+++ tests/UnitTest_1.py
@@ -1,19 +1,42 @@
 import Anime
+import pytest
+from db.exc import NoResultFound, IntegrityError
+import i_db as Database
+
+db: Database = Database.Database()
 
 
-class getAnime_UnitTest:
+class GET_Anime_UnitTest:
 
-    db = None
+    retrieved_anime: Anime = Anime(id=1, title="Naruto",
+                                   release_date='2020-11-10 18:30:00')
 
-    id: int = 1
-    retrieved_anime: Anime = None
-
-    def setUp(self):
-        self.retrieved_anime = db.getAnime(id)
-
-    def getExistingAnimeFromDB(self):
+    def GET_ExistingAnimeFromDB(self):
 
         expectedAnime: Anime = Anime(id=1, title="Naruto",
                                      release_date='2020-11-10 18:30:00')
 
         assert (self.retrieved_anime == expectedAnime)
+
+    def GET_NonExistentAnimeFromDB(self):
+        with pytest.raises(NoResultFound):
+            db.get_anime_by_id(99999)
+
+
+class POST_Anime_UnitTest:
+
+    new_anime: Anime = Anime(id=1, title="Naruto",
+                             release_date='2020-11-10 18:30:00')
+
+    def tearDown(self):
+        db.dev.clean()
+
+    def POST_NonExistentAnimeFromDB(self):
+        db.post_anime(new_anime)
+        retrieved_anime: Anime = db.get_anime_by_id(new_anime.id)
+        assert (retrieved_anime == new_anime)
+
+    def POST_ExistingAnimeFromDB(self):
+        db.post_anime(new_anime)
+        with pytest.raises(IntegrityError):
+            db.post_anime(new_anime)
